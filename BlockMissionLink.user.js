b0d091245db96ffdfaa83288139ed574c82316b3
XFreyaX <43953090+XFreyaX@users.noreply.github.com>
2019-01-30 12:31:26 +0100

    Create BlockMissionLink.user.js

---
+++ BlockMissionLink.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         BlockMissionLink
+// @namespace    Leitstellenspiel
+// @version      0.3
+// @description  Verlinke Einsätze im Chat ausblenden
+// @author       x_Freya_x
+// @include      https://www.leitstellenspiel.de/*
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    var circle = 'width: 20px; height: 20px; border: 1px solid blue; text-align: center; border-radius: 20px;';
+    var i;
+
+    $(".nav.navbar-nav.navbar-right").not(".hidden-xs").append('<li><a id="blockchatmission" class="blockchatmission"><div id="blue_circle" style="background-color: rgb(0, 0, 255);' + circle + '"><span id="filter" class="glyphicon glyphicon-fire"></span></div></a></li>');
+
+    $("#blockchatmission").click(function() {
+        if($('#blue_circle').css('background-color') == 'rgb(0, 0, 255)'){
+            $("#blue_circle").css({'background-color':'grey'});
+        } else {
+            $("#blue_circle").css({'background-color':'rgb(0, 0, 255)'});
+        }
+    })
+
+    function refreshData()
+    {
+        const x = 10;
+        if($('#blue_circle').css('background-color') !== 'rgb(0, 0, 255)'){
+            let cl = document.getElementById('mission_chat_messages');
+            let cll = cl.children.length;
+
+            for (i = 0; i < cll; i++) {
+                let it = cl.children[i];
+                let ih = it.innerHTML;
+                let ii = ih.indexOf('/missions/');
+                if (ii !== -1) {
+                        it.style.display = 'none';
+                }
+            }
+        } else {
+            let cl = document.getElementById('mission_chat_messages');
+            let cll = cl.children.length;
+
+            for (i = 0; i < cll; i++) {
+                let it = cl.children[i];
+                let ih = it.innerHTML;
+                let ii = ih.indexOf('/missions/');
+                if (ii !== -1) {
+                        it.style.display = '';
+                }
+            }
+        }
+
+        setTimeout(refreshData, x*1000);
+    }
+
+    if($('#blue_circle').css('background-color') !== 'rgb(0, 0, 255)'){
+        let cl = document.getElementById('mission_chat_messages');
+        let cll = cl.children.length;
+
+        for (i = 0; i < cll; i++) {
+            let it = cl.children[i];
+            let ih = it.innerHTML;
+            let ii = ih.indexOf('/missions/');
+            if (ii !== -1) {
+                it.style.display = 'none';
+            }
+        }
+    } else {
+        let cl = document.getElementById('mission_chat_messages');
+        let cll = cl.children.length;
+
+        for (i = 0; i < cll; i++) {
+            let it = cl.children[i];
+            let ih = it.innerHTML;
+            let ii = ih.indexOf('/missions/');
+            if (ii !== -1) {
+                it.style.display = '';
+            }
+        }
+    }
+
+    refreshData();
+
+})
+();
