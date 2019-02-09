0216211e66e0346ef05a3470311c139036f38bfc
XFreyaX <43953090+XFreyaX@users.noreply.github.com>
2019-02-09 17:12:46 +0100

    Update BlockMissionLink.user.js

--- BlockMissionLink.user.js
+++ BlockMissionLink.user.js
@@ -1,10 +1,16 @@
 // ==UserScript==
 // @name         BlockMissionLink
 // @namespace    Leitstellenspiel
-// @version      0.4
+// @version      0.5
 // @description  Verlinke Einsätze im Chat ausblenden
 // @author       x_Freya_x
 // @include      https://www.leitstellenspiel.de/*
+// @exclude      https://www.leitstellenspiel.de/verband*
+// @exclude      https://www.leitstellenspiel.de/alliance_threads*
+// @exclude      https://www.leitstellenspiel.de/alliances*
+// @exclude      https://www.leitstellenspiel.de/alliance_logfiles*
+// @exclude      https://www.leitstellenspiel.de/schoolings*
+// @require      https://code.jquery.com/jquery-3.3.1.js
 // ==/UserScript==
 
 (() => {
@@ -12,6 +18,7 @@
 
     var circle = 'width: 20px; height: 20px; border: 1px solid blue; text-align: center; border-radius: 20px;';
     var i;
+    var timerId;
 
     $(".nav.navbar-nav.navbar-right").not(".hidden-xs").append('<li><a id="blockchatmission" class="blockchatmission"><div id="blue_circle" style="background-color: rgb(0, 0, 255);' + circle + '"><span id="filter" class="glyphicon glyphicon-fire"></span></div></a></li>');
 
@@ -25,10 +32,10 @@
 
     function refreshData()
     {
-        if($('#blue_circle').css('background-color') !== 'rgb(0, 0, 255)'){
-            let cl = document.getElementById('mission_chat_messages');
-            let cll = cl.children.length;
+        let cl = document.getElementById('mission_chat_messages');
+        let cll = cl.children.length;
 
+        if($('#blue_circle').css('background-color') !== 'rgb(0, 0, 255)'){
             for (i = 0; i < cll; i++) {
                 let it = cl.children[i];
                 let ih = it.innerHTML;
@@ -38,9 +45,6 @@
                 }
             }
         } else {
-            let cl = document.getElementById('mission_chat_messages');
-            let cll = cl.children.length;
-
             for (i = 0; i < cll; i++) {
                 let it = cl.children[i];
                 let ih = it.innerHTML;
